bf18e90acbaf88181635c1571606671374247a4b
pr0grammerMahin <nimahin25@gmail.com>
2024-05-02 20:40:49 +0600

    added

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+<script src="<?php echo get_template_directory_uri() ?>/js/index.js"></script>
+<footer class="footer">
+    <div class="footer-widget-area">
+        <?php dynamic_sidebar('footer_sidebar') ?>
+    </div>
+</footer>
+<?php wp_footer() ?>
+</body>
+</html>
